e9a72c2270cea209f491ffb6217a3f59fac16076
donia-elsayed <dunia2016170569@cis.asu.edu.eg>
2024-09-04 17:54:25 +0300

    Add main components

---
+++ src/components/CreateProductForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateProductForm = () => {
+  return <div>CreateProductForm</div>;
+};
+
+export default CreateProductForm;
---
+++ src/components/EditOrderForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditOrderForm = () => {
+  return <div>EditOrderForm</div>;
+};
+
+export default EditOrderForm;
