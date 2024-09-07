9ee33f45e611e036b2a4936649740f670423da92
donia-elsayed <dunia2016170569@cis.asu.edu.eg>
2024-09-07 21:19:43 +0300

    Add Toaster

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
       <App />
+      <Toaster position="top-center" />
     </Router>
   </React.StrictMode>
 );
