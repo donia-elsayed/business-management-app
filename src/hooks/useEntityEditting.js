b75a91adc349af1229175c35e93034336c794057
donia-elsayed <dunia2016170569@cis.asu.edu.eg>
2024-09-08 00:57:58 +0300

    remove unneeded code

--- src/hooks/useEntityEditting.js
+++ src/hooks/useEntityEditting.js
@@ -1,10 +1,10 @@
 export const useEntityEditing = ({ setEntities, entityType }) => {
   const updateEntity = (id, updatedFields) => {
-    console.log(id);
     setEntities((prevEntities) => {
       const updatedEntities = prevEntities.map((entity) =>
         entity.id === id ? { ...entity, ...updatedFields } : entity
       );
+
       localStorage.setItem(entityType, JSON.stringify(updatedEntities));
       return updatedEntities;
     });
