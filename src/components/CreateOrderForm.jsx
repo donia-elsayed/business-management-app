c047838e2a7e4b65e178363b426ac23c59f7d73a
donia-elsayed <dunia2016170569@cis.asu.edu.eg>
2024-09-08 02:43:52 +0300

    update create order form style

--- src/components/CreateOrderForm.jsx
+++ src/components/CreateOrderForm.jsx
@@ -30,7 +30,7 @@ const CreateOrderForm = ({ onSave }) => {
     resetForm();
   };
   return (
-    <div className="w-full max-w-md mx-auto bg-white p-8 rounded-lg shadow-md">
+    <div className="w-full max-w-md mx-auto bg-white px-4 py-6 rounded-lg shadow-md">
       <Formik
         initialValues={initialOrderValues}
         validationSchema={validationOrderSchema}
@@ -172,7 +172,7 @@ const CreateOrderForm = ({ onSave }) => {
           </div>
           <button
             type="submit"
-            className="w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700"
+            className="block w-full px-4 py-2 text-sm font-medium text-white bg-gradient-to-r from-purple-500 to-pink-500 border border-transparent rounded-md shadow-sm hover:from-purple-600 hover:to-pink-600"
           >
             Create New Order
           </button>
