2b090b0b0faedabd52c53584ec2a8daec7370967
donia-elsayed <dunia2016170569@cis.asu.edu.eg>
2024-09-08 01:00:29 +0300

    update table code to handle item id

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { TrashIcon, PencilSquareIcon } from "@heroicons/react/20/solid";
-import { getStatusClasses } from "../utitlities/statusStyles";
+import { getStatusClasses } from "../utitlities/utilityStyle";
 
 const Table = ({ headers, data = [], onEdit, onDelete, onToggleBlock }) => {
   const showImageHeader = headers.includes("image");
@@ -27,64 +27,68 @@ const Table = ({ headers, data = [], onEdit, onDelete, onToggleBlock }) => {
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
           {data.length > 0 &&
-            data?.map((item) => (
-              <tr key={item.id}>
-                {showImageHeader && (
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <img
-                      src={item.image}
-                      alt={item.name || "Product Image"}
-                      className="h-15 w-15 object-cover rounded"
-                    />
-                  </td>
-                )}
-                {headers.map((header) => (
-                  <td
-                    key={header}
-                    className="px-6 py-4 whitespace-nowrap text-base"
-                  >
-                    <span
-                      className={`${
-                        header === "status"
-                          ? getStatusClasses(item[header])
-                          : ""
-                      } px-3 py-2 rounded-full`}
-                    >
-                      {item[header]}
-                    </span>
-                  </td>
-                ))}
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                  <div className="flex space-x-2">
-                    {onEdit && (
-                      <button
-                        onClick={() => onEdit(item)}
-                        className="outline outline-1 outline-gray-200 px-3 py-2 text-blue-600 hover:text-blue-900 mr-2 rounded-md"
-                      >
-                        <PencilSquareIcon className="h-6 w-6" />
-                      </button>
-                    )}
-                    {onDelete && (
-                      <button
-                        onClick={() => onDelete(item.id)}
-                        className="outline outline-1 outline-gray-200 px-3 py-2 text-red-600 hover:text-red-900 mr-2 rounded-md"
-                      >
-                        <TrashIcon className="h-6 w-6" />
-                      </button>
-                    )}
-                  </div>
-
-                  {onToggleBlock && (
-                    <button
-                      onClick={() => onToggleBlock(item.id, item.isBlocked)}
-                      className={`${item.isBlocked} ? 'text-green-600 hover:text-green-900' : 'text-red-600 hover:text-red-900'`}
-                    >
-                      {item.isBlocked ? "Unblock" : "Block"}
-                    </button>
+            data?.map((item) => {
+              if (!item) return null;
+              const itemId = item.id;
+              return (
+                <tr key={itemId}>
+                  {showImageHeader && (
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <img
+                        src={item.image}
+                        alt={item.name || "Product Image"}
+                        className="h-15 w-15 object-cover rounded"
+                      />
+                    </td>
                   )}
-                </td>
-              </tr>
-            ))}
+                  {headers.map((header) => (
+                    <td
+                      key={header}
+                      className="px-6 py-4 whitespace-nowrap text-base"
+                    >
+                      <span
+                        className={`${
+                          header === "status"
+                            ? getStatusClasses(item[header])
+                            : ""
+                        } px-3 py-2 rounded-full`}
+                      >
+                        {item[header]}
+                      </span>
+                    </td>
+                  ))}
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                    <div className="flex space-x-2">
+                      {onEdit && (
+                        <button
+                          onClick={() => onEdit(item)}
+                          className="outline outline-1 outline-gray-200 px-3 py-2 text-blue-600 hover:text-blue-900 mr-2 rounded-md"
+                        >
+                          <PencilSquareIcon className="h-6 w-6" />
+                        </button>
+                      )}
+                      {onDelete && (
+                        <button
+                          onClick={() => onDelete(item.id)}
+                          className="outline outline-1 outline-gray-200 px-3 py-2 text-red-600 hover:text-red-900 mr-2 rounded-md"
+                        >
+                          <TrashIcon className="h-6 w-6" />
+                        </button>
+                      )}
+                    </div>
+
+                    {onToggleBlock && (
+                      <button
+                        onClick={() => onToggleBlock(item.id, item.isBlocked)}
+                        className={`${item.isBlocked} ? 'text-green-600 hover:text-green-900' : 'text-red-600 hover:text-red-900'`}
+                      >
+                        {item.isBlocked ? "Unblock" : "Block"}
+                      </button>
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     </div>
