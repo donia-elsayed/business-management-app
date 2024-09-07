298466df9fb4bb01a429d738ebf0f128198ad4d9
unknown <bebowagih2000@gmail.com>
2024-09-07 17:21:45 +0300

    use toaster in route

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { Toaster } from "react-hot-toast";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <Toaster position='top-center' reverseOrder={false} />
     <Router>
       <App />
+      <Toaster position="top-center" />
     </Router>
   </React.StrictMode>
 );
