a44527474e7453ce0145f42f17bd3a73c45cbe5f
donia-elsayed <dunia2016170569@cis.asu.edu.eg>
2024-09-08 12:48:24 +0300

    Update product page with new handling for modal,edit,delete product

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,37 +1,48 @@
 import { useState } from "react";
 import Table from "../components/Table";
-import { EditProductModal } from "../components/EditProductModal";
 import { useFetchProducts } from "../hooks/useFetchProducts";
 import { showToast } from "../utitlities/utilityStyle";
-
+import { useModal } from "../hooks/useModal";
+import { useEntityEditing } from "../hooks/useEntityEditting";
+import { useEntityDeletion } from "../hooks/useEntityDeletion";
+import { PlusCircleIcon } from "@heroicons/react/20/solid";
+import Modal from "../components/Modal";
+import EditProductForm from "../components/EditProductForm";
+import CreateProductForm from "../components/CreateProductForm";
 const ProductsPage = () => {
-  const { products, error, loading } = useFetchProducts();
+  const { products, setProducts, error, loading } = useFetchProducts();
   const [selectedProduct, setSelectedProduct] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [localProducts, setLocalProducts] = useState(products);
+  const { isModalOpen, modalType, openModal, closeModal } = useModal();
 
+  const { updateEntity: updateProduct } = useEntityEditing({
+    setEntities: setProducts,
+    entityType: "products",
+  });
+  const { removeEntity: deleteProduct } = useEntityDeletion({
+    setEntities: setProducts,
+    entityType: "products",
+  });
   const handleEdit = (product) => {
     setSelectedProduct(product);
-    setIsModalOpen(true);
+    openModal("edit");
   };
-
   const handleDelete = (id) => {
-    const updatedProducts = localProducts.filter(
-      (product) => product.id !== id
-    );
-    setLocalProducts(updatedProducts);
-    localStorage.setItem("products", JSON.stringify(updatedProducts));
-    showToast("Product Deleted Successfully!", "success");
+    deleteProduct(id);
+    showToast("Product deleted Successfully!", "success");
   };
 
-  const handleSave = (updatedProduct) => {
-    const updatedProducts = localProducts.map((product) =>
-      product.id === updatedProduct.id ? updatedProduct : product
-    );
-    setLocalProducts(updatedProducts);
-    localStorage.setItem("products", JSON.stringify(updatedProducts));
-    setIsModalOpen(false);
-    showToast("Product Updated Successfully!", "success");
+  const handleSave = (selectedProduct, updatedProduct) => {
+    if (modalType === "edit" && selectedProduct) {
+      updateProduct(selectedProduct.id, updatedProduct);
+      showToast("Product updated successfully!", "success");
+    } else {
+      setProducts((prevProducts) => [
+        ...prevProducts,
+        { id: prevProducts.length + 1, ...updatedProduct },
+      ]);
+      showToast("Product created successfully!", "success");
+    }
+    closeModal();
   };
 
   const headers = ["title", "price", "description", "category", "images"];
@@ -41,20 +52,32 @@ const ProductsPage = () => {
 
   return (
     <div className="container mx-auto p-4">
+      <div className="flex justify-between items-center px-8 m-auto">
+        <h2 className="text-2xl font-bold text-gray-600">Products</h2>
+        <button
+          onClick={() => openModal("create")}
+          className="flex gap-2 bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 text-white font-bold py-3 px-6 rounded-lg hover:shadow-lg my-6"
+        >
+          <PlusCircleIcon className="h-6 w-6" /> <span>Create New Product</span>
+        </button>
+      </div>
       <Table
         headers={headers}
-        data={localProducts}
+        data={products}
         onEdit={handleEdit}
         onDelete={handleDelete}
       />
-      {selectedProduct && (
-        <EditProductModal
-          product={selectedProduct}
-          isOpen={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-          onSave={handleSave}
-        />
-      )}
+      <Modal
+        isOpen={isModalOpen}
+        closeModal={closeModal}
+        title={modalType === "edit" ? "Edit Product" : "Create New Product"}
+      >
+        {modalType === "edit" ? (
+          <EditProductForm product={selectedProduct} onSave={handleSave} />
+        ) : (
+          <CreateProductForm onSave={handleSave} />
+        )}
+      </Modal>
     </div>
   );
 };
