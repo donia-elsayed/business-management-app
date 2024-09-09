17b78226955460d794059131f4fec72d7bc5ffce
donia-elsayed <dunia2016170569@cis.asu.edu.eg>
2024-09-09 11:55:58 +0300

    Add user data

---
+++ src/data/users.js
@@ -0,0 +1,62 @@
+export const usersData = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe@example.com",
+    userStatus: "Active",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    email: "janesmith@example.com",
+    userStatus: "Blocked",
+  },
+  {
+    id: 3,
+    name: "Michael Johnson",
+    email: "michaelj@example.com",
+    userStatus: "Active",
+  },
+  {
+    id: 4,
+    name: "Emily Davis",
+    email: "emilydavis@example.com",
+    userStatus: "Blocked",
+  },
+  {
+    id: 5,
+    name: "Daniel Brown",
+    email: "danielbrown@example.com",
+    userStatus: "Active",
+  },
+  {
+    id: 6,
+    name: "Sarah Wilson",
+    email: "sarahwilson@example.com",
+    userStatus: "Blocked",
+  },
+  {
+    id: 7,
+    name: "David Clark",
+    email: "davidclark@example.com",
+    userStatus: "Active",
+  },
+  {
+    id: 8,
+    name: "Laura Martinez",
+    email: "lauramartinez@example.com",
+    userStatus: "Active",
+  },
+  {
+    id: 9,
+    name: "James Miller",
+    email: "jamesmiller@example.com",
+    userStatus: "Blocked",
+  },
+  {
+    id: 10,
+    name: "Sophia Garcia",
+    email: "sophiagarcia@example.com",
+    userStatus: "Active",
+  },
+];
