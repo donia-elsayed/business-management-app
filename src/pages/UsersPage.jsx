d695ea6030077899f70b809f08e8eddcb81f9487
donia-elsayed <dunia2016170569@cis.asu.edu.eg>
2024-09-09 11:57:13 +0300

    Add user page to display users and handle block and unblock

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { usersData } from "../data/users";
+import Table from "../components/Table";
 
 const UsersPage = () => {
-  return <div></div>;
+  const headers = ["name", "email", "userStatus"];
+  const [users, setUsers] = useState([]);
+  const handleBlock = (id, currentStatus) => {
+    const updatedUsers = users.map((user) =>
+      user.id === id
+        ? {
+            ...user,
+            userStatus: currentStatus === "Active" ? "Blocked" : "Active",
+          }
+        : user
+    );
+
+    setUsers(updatedUsers);
+    localStorage.setItem("users", JSON.stringify(updatedUsers));
+  };
+
+  useEffect(() => {
+    const storedUsers = JSON.parse(localStorage.getItem("users")) || usersData;
+    setUsers(storedUsers);
+    localStorage.setItem("users", JSON.stringify(storedUsers));
+  }, []);
+
+  return (
+    <div>
+      <Table headers={headers} data={users} onToggleBlock={handleBlock} />
+    </div>
+  );
 };
 
 export default UsersPage;
