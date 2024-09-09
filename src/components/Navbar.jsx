162517d767800b0ec6da42eeaac071e5c06a78c5
donia-elsayed <dunia2016170569@cis.asu.edu.eg>
2024-09-09 14:13:32 +0300

    handle show logout when user loggedin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ user }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isAuthPage =
+    location.pathname === "/login" || location.pathname === "/register";
+  const handleLogout = () => {
+    localStorage.removeItem("loggedUser");
+    navigate("/login");
+  };
   return (
     <nav className="text-white p-4 border-b-[0.5px] border-gray-100 bg-gradient-to-r from-[#0072ff] to-[#00c6ff]">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-lg sm:text-xl font-bold">Business Management</h1>
-        <div className="flex space-x-4">
-          <Link to="/login" className="px-2 sm:px-4 text-sm sm:text-base font-bold hover:text-gray-50" aria-label="Login">Login</Link>
-          <Link to="/register" className="px-2 sm:px-4 text-sm sm:text-base font-bold hover:text-gray-50" aria-label="Register">Register</Link>
-        </div>
+        {user && !isAuthPage ? (
+          <button
+            onClick={handleLogout}
+            className="px-2 sm:px-4 text-sm sm:text-base font-bold hover:text-gray-50"
+            aria-label="Logout"
+          >
+            Logout
+          </button>
+        ) : (
+          <div className="flex space-x-4">
+            <Link
+              to="/login"
+              className="px-2 sm:px-4 text-sm sm:text-base font-bold hover:text-gray-50"
+              aria-label="Login"
+            >
+              Login
+            </Link>
+            <Link
+              to="/register"
+              className="px-2 sm:px-4 text-sm sm:text-base font-bold hover:text-gray-50"
+              aria-label="Register"
+            >
+              Register
+            </Link>
+          </div>
+        )}
       </div>
     </nav>
   );
 };
 
 export default Navbar;
-
-
