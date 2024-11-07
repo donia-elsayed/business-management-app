58377875ca7281269935b4669c20485065bde879
donia-elsayed <dunia2016170569@cis.asu.edu.eg>
2024-11-08 00:09:35 +0200

    add base name for react route

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Toaster } from "react-hot-toast";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/business-management-app">
       <App />
       <Toaster position="top-center" />
     </Router>
